cc2e0d4c5f26a7ee32490f3d20952887597d7a4c
Mustafa <pourmhere@gmail.com>
2024-09-13 21:13:31 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,23 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [str, setStr] = useState("");
+  // let str = "";
+  function handleClick(e) {
+    setStr("Hello " + e.target.value + "!");
+    console.log(str);
+  }
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      Enter you name:
+      <br />
+      <br />
+      <input type="text" onChange={handleClick} />
+      <p>{str}</p>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
